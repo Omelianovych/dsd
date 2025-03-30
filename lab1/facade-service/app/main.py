de8e9c49e7d830e61d27bb4f4ef60cde446ce3ec
perry <perry@kali.perry.com>
2025-03-30 04:01:52 +0300

    lab1 all

--- lab1/facade-service/app/main.py
+++ lab1/facade-service/app/main.py
@@ -4,6 +4,7 @@ import logging_pb2
 import messages_pb2
 import uuid
 from pydantic import BaseModel
+from tenacity import retry, wait_fixed, stop_after_attempt, RetryError
 
 app = FastAPI()
 
@@ -13,17 +14,28 @@ messages_stub = grpc_client.get_messages_client()
 class MessageRequest(BaseModel):
     message: str
 
+@retry(wait=wait_fixed(2), stop=stop_after_attempt(5))
+def send_log_message_to_logging_service(message_id: str, message: str):
+    try:
+        logging_stub = grpc_client.get_logging_client()
+        response = logging_stub.LogMessage(logging_pb2.LogRequest(uuid=message_id, message=message))
+        return response
+    except Exception as e:
+        print(e)
+        raise
+
 @app.post("/send")
 def send_message(request: MessageRequest):
-    message_id = str(uuid.uuid4())
-    response = logging_stub.LogMessage(logging_pb2.LogRequest(uuid=message_id, message=request.message))  # Отправляем в logging-service
-
-    return {"success": response.success, "uuid": message_id, "message": request.message}
+    message_id = str(uuid.uuid4())  # Генерируем UUID
+    try:
+        response = send_log_message_to_logging_service(message_id, request.message)
+        return {"success": response.success, "uuid": message_id, "message": request.message}
+    except RetryError as e:
+        return {"error": "Failed to send message after multiple attempts", "message": str(e)}
 
 @app.get("/fetch")
 def fetch_messages():
     log_response = logging_stub.GetAllMessages(logging_pb2.Empty())
     msg_response = messages_stub.GetStaticMessage(messages_pb2.Empty())
     combined_response = ", ".join(log_response.messages) + ", " + msg_response.message
-    # combined_response = msg_response.message
     return {"messages": combined_response}
--- lab1/logging-service/app/main.py
+++ lab1/logging-service/app/main.py
@@ -5,14 +5,24 @@ import logging_pb2_grpc
 
 
 messages_dict = {}
+processed_uuids = set()
 
 class LoggingService(logging_pb2_grpc.LoggingServiceServicer):
     def LogMessage(self, request, context):
+
+        if request.uuid in processed_uuids:
+            print("[DUPLICATE]")
+            return logging_pb2.LogResponse(success=False)
+
         messages_dict[request.uuid] = request.message
+        processed_uuids.add(request.uuid)
+        print(f"[SAVED] {request.uuid}: {request.message}")
         return logging_pb2.LogResponse(success=True)
 
     def GetAllMessages(self, request, context):
         return logging_pb2.MessagesResponse(messages=list(messages_dict.values()))
+
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
